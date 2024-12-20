bb83105d7cc1072a4ef9838c80bae1b8e196a0fe
stemyy <Himanbro112233@gmail.com>
2024-12-19 23:37:15 -0500

    basic GUI

---
+++ src/Main/Game.java
@@ -0,0 +1,22 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame{
+    public Game(){
+        setSize(640,640);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        GameScreen gameScreen = new GameScreen();
+        add(gameScreen);
+        setVisible(true);
+        
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+}
+
---
+++ src/Main/GameScreen.java
@@ -0,0 +1,17 @@
+package Main;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GameScreen extends JPanel{
+    public GameScreen(){
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillRect(50, 50, 100, 100);
+        g.drawRect(50, 50, 100, 100);
+    }
+}
