adff3fd45f9fe01a66062896745be8e059ba4868
Himanbro112233@gmail.com <Himanbro112233@gmail.com>
2024-12-20 17:08:48 -0500

    cool thing

--- src/Main/GameScreen.java
+++ src/Main/GameScreen.java
@@ -3,23 +3,28 @@ package Main;
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScreen extends JPanel{ 
     private Random random;
     private BufferedImage img;
+    private ArrayList <BufferedImage> sprites = new ArrayList<>()
 
     public GameScreen(BufferedImage img){
         this.img = img;
         random = new Random();
     }
 
+    private void LoadSprites(){
+
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.drawImage(img, 0, 0, null);
+        g.drawImage(img.getSubimage(0, 32, 32, 32), 0, 0, null);
     }
-
     public Color randColor(){
         int r = random.nextInt(256);
         int g = random.nextInt(256);
