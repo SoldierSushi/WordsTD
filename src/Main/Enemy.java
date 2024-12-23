6c472558c16aa24edf11d67804d66f0c39eee455
Himanbro112233@gmail.com <Himanbro112233@gmail.com>
2024-12-23 14:23:41 -0500

    added enemy class

---
+++ src/Main/Enemy.java
@@ -0,0 +1,74 @@
+package Main;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Enemy {
+    private int enemyX = 0;
+    private int enemyY = 64;
+    private BufferedImage image;
+    private int mapX = 0, mapY = 0;
+    private int[][] enemyPathMap = {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {1,2,3,4,5,0,27,28,29,30,31,0,0},
+        {0,0,0,0,6,0,26,0,0,0,32,0,0},
+        {0,10,9,8,7,0,25,0,0,0,33,0,0},
+        {0,11,0,0,0,0,24,0,0,0,34,0,0},
+        {0,12,0,0,0,0,23,0,0,0,35,0,0},
+        {0,13,0,0,20,21,22,0,0,0,36,0,0},
+        {0,14,0,0,19,0,0,0,39,38,37,0,0},
+        {0,15,16,17,18,0,0,0,40,0,0,0,0},
+        {0,0,0,0,0,0,0,0,41,0,0,0,0},
+        {0,49,48,47,46,45,44,43,42,0,0,0,0},
+        {0,50,0,0,0,0,0,0,0,0,0,0,0},
+        {0,51,52,53,54,55,56,57,58,59,60,61,62},
+    };
+
+    public Enemy(int enemyX, int enemyY, BufferedImage image) {
+        this.enemyX = enemyX;
+        this.enemyY = enemyY;
+        this.image = image;
+        this.mapX = enemyX / 64;
+        this.mapY = enemyY / 64;
+    }
+
+    public boolean update() {
+        // Convert grid position to pixel position
+        int targetX = mapX * 64;
+        int targetY = mapY * 64;
+    
+        // Move toward the target pixel position
+        if (enemyX < targetX) enemyX += 8;
+        if (enemyX > targetX) enemyX -= 8;
+        if (enemyY < targetY) enemyY += 8;
+        if (enemyY > targetY) enemyY -= 8;
+    
+        // If reached target cell, find the next cell
+        if(enemyPathMap[mapY][mapX] != 62){
+            if (enemyX == targetX && enemyY == targetY) {
+                findNextCell();
+            }
+        }else{
+            return true;
+        }
+        return false;
+    }
+
+    private void findNextCell() {
+        // Check adjacent cells in a fixed order: right, down, left, up
+        if (enemyPathMap[mapY][mapX + 1] > enemyPathMap[mapY][mapX]) {
+            mapX += 1; // Move right
+        } else if (enemyPathMap[mapY + 1][mapX] > enemyPathMap[mapY][mapX]) {
+            mapY += 1; // Move down
+        } else if (enemyPathMap[mapY][mapX - 1] > enemyPathMap[mapY][mapX]) {
+            mapX -= 1; // Move left
+        } else if (enemyPathMap[mapY - 1][mapX] > enemyPathMap[mapY][mapX]) {
+            mapY -= 1; // Move up
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(image, enemyX, enemyY, null);
+    }
+
+}
--- src/Main/GameScreen.java
+++ src/Main/GameScreen.java
@@ -4,13 +4,11 @@ import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class GameScreen extends JPanel implements Runnable{ 
     private BufferedImage img;
     private long fps = 0;
-    private int enemyX = 0;
-    private int enemyY = 64;
-    private int enemyCurrentPos = 1;
     private long lastTime;
     private long lastFrameTime;
     Thread fpsThread;
@@ -32,44 +30,19 @@ public class GameScreen extends JPanel implements Runnable{
         {0,1,0,0,0,0,0,0,0,0,0,0,0},
         {0,1,1,1,1,1,1,1,1,1,1,1,9},
     };
-    private int[][] enemyPathMap = {
-        {0,0,0,0,0,0,0,0,0,0,0,0,0},
-        {1,2,3,4,5,0,27,28,29,30,31,0,0},
-        {0,0,0,0,6,0,26,0,0,0,32,0,0},
-        {0,10,9,8,7,0,25,0,0,0,33,0,0},
-        {0,11,0,0,0,0,24,0,0,0,34,0,0},
-        {0,12,0,0,0,0,23,0,0,0,35,0,0},
-        {0,13,0,0,20,21,22,0,0,0,36,0,0},
-        {0,14,0,0,19,0,0,0,39,38,37,0,0},
-        {0,15,16,17,18,0,0,0,40,0,0,0,0},
-        {0,0,0,0,0,0,0,0,41,0,0,0,0},
-        {0,49,48,47,46,45,44,43,42,0,0,0,0},
-        {0,50,0,0,0,0,0,0,0,0,0,0,0},
-        {0,51,52,53,54,55,56,57,58,59,60,61},
-    };
-    private int mapX = 0, mapY = 0;
+
+    private ArrayList<Enemy> enemies;
 
     public GameScreen(BufferedImage img) {
         lastTime = System.nanoTime();
         lastFrameTime = System.nanoTime();
         this.img = img;
         preloadBackground();
-    
-        // Find starting position in the map
-        for (int y = 0; y <= size; y++) {
-            for (int x = 0; x < size; x++) {
-                if (map[y][x] == 1) {
-                    mapX = x;
-                    mapY = y;
-                    enemyX = mapX * 64;
-                    enemyY = mapY * 64;
-                    break;
-                }
-            }
-            if (map[mapY][mapX] == 1) break;
-        }
-    
         fpsThreadStart();
+
+        // Initialize enemies
+        enemies = new ArrayList<>();
+        enemies.add(new Enemy(0, 64, img.getSubimage(20 * 64, 6 * 64, 64, 64)));
     }
 
     @Override
@@ -77,7 +50,7 @@ public class GameScreen extends JPanel implements Runnable{
         while (true) {
             long currentTime = System.nanoTime(); //finds the last time the frame needs to be changed
             if (System.nanoTime() - lastTime >= timePerFrame) {
-                updateEnemy(); // Update logic
+                updateEnemies(); // Update logic
                 repaint(); // Request a repaint
                 lastTime = currentTime; // Reset frame timer
             }
@@ -94,10 +67,12 @@ public class GameScreen extends JPanel implements Runnable{
             }
         }
 
-        //draw characters
-        g.drawImage(img.getSubimage(20*64, 6*64, 64, 64), enemyX, enemyY, null);
+        // Draw all enemies
+        for (Enemy enemy : enemies) {
+            enemy.draw(g);
+        }
 
-        updateShit();
+        updateStuff();
     }
 
     public void preloadBackground(){
@@ -122,7 +97,7 @@ public class GameScreen extends JPanel implements Runnable{
         fpsThread.start();
     }
 
-    private void updateShit() {
+    private void updateStuff() {
         fps++;
         long currentFrameTime = System.nanoTime();
         if (currentFrameTime - lastFrameTime >= 1000000000) { // One second has passed
@@ -132,35 +107,16 @@ public class GameScreen extends JPanel implements Runnable{
         }
     }
 
-    private void updateEnemy() {
-        // Convert grid position to pixel position
-        int targetX = mapX * 64;
-        int targetY = mapY * 64;
-    
-        // Move toward the target pixel position
-        if (enemyX < targetX) enemyX += 8;
-        if (enemyX > targetX) enemyX -= 8;
-        if (enemyY < targetY) enemyY += 8;
-        if (enemyY > targetY) enemyY -= 8;
-    
-        // If reached target cell, find the next cell
-        if(enemyPathMap[mapY][mapX] != 61){
-            if (enemyX == targetX && enemyY == targetY) {
-                findNextCell();
+    private void updateEnemies() {
+        // Use an iterator to safely remove enemies
+        Iterator<Enemy> iterator = enemies.iterator();
+        while (iterator.hasNext()) {
+            Enemy enemy = iterator.next();
+            if (enemy.update()) {
+                // Remove enemy if it reached the end goal
+                iterator.remove();
+                System.out.println("Enemy reached the end and was removed.");
             }
-        }
-    }
-
-    private void findNextCell() {
-        // Check adjacent cells in a fixed order: right, down, left, up
-        if (enemyPathMap[mapY][mapX + 1] > enemyPathMap[mapY][mapX]) {
-            mapX += 1; // Move right
-        } else if (enemyPathMap[mapY + 1][mapX] > enemyPathMap[mapY][mapX]) {
-            mapY += 1; // Move down
-        } else if (enemyPathMap[mapY][mapX - 1] > enemyPathMap[mapY][mapX]) {
-            mapX -= 1; // Move left
-        } else if (enemyPathMap[mapY - 1][mapX] > enemyPathMap[mapY][mapX]) {
-            mapY -= 1; // Move up
         }
     }
 }
